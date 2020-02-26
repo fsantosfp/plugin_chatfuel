acab56db48435dda84c807176fd234f51d4e3d59
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-02-26 17:27:12 -0300

    Incluido parametros do chatfuel intent + endpoint

--- web/backend/controller/IntentsController.js
+++ web/backend/controller/IntentsController.js
@@ -1,21 +1,28 @@
 const Dialogflow = require('../service/DialogflowAPI')
 const GoogleCloud = require('../service/GoogleCloud')
+const Storage = require('../../../app/json/Storage')
 
 exports.intentsList = async (req, res)=>{
 
     const projectId = req.params.id
     const token = req.session.token
 
+    console.log(req.session, token)
+
     const googleCloud = new GoogleCloud
     const api = new Dialogflow(token)
 
     const projects = await googleCloud.getProjects(token)
     const intents = await api.intentsList(projectId)
-
+    const file = new Storage(projectId);
+    const chatfuel = file.getFile(projectId)
+    
     const params = {
         projects: projects,
         intents: intents,
-        projectId: projectId
+        projectId: projectId,
+        chatfuel: chatfuel.intents,
+        endpoint: chatfuel.url
     }
 
     res.render('pages/intents', {params: params})
