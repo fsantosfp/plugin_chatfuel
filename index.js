b7ea971ce17b4da49daf1269bbaa6613bc4e0491
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-02-26 17:16:33 -0300

    Alterado nomenclatura e adicionado rota de api

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 
 const AuthRouter = require('./web/backend/routes/Authentication')
 const HomeRouter = require('./web/backend/routes/Home')
-const APIRouter = require('./web/backend/routes/Intents')
+const IntentsRouter = require('./web/backend/routes/Intents')
+
+const APIRouter = require('./app/routes/API')
 
 const app = express()
 app.use(express.static('web/public'))
@@ -17,6 +19,7 @@ app.use(session({secret: 'teste',saveUninitialized: true,resave: true}))
 
 app.use(AuthRouter)
 app.use(HomeRouter)
+app.use(IntentsRouter)
 app.use(APIRouter)
 
 app.set('views', path.join(__dirname,'web/backend/views'))
