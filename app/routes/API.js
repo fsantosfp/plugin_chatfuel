85a2a525c4b1eb7c338ef0c9eec7e8d1ee1dba63
unknown <fsantos.fps@gmail.com>
2020-02-27 01:03:12 -0300

    Adicionado rotas para detectIntent

--- app/routes/API.js
+++ app/routes/API.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const JSONController = require('../controller/JSONController')
+const IntentController = require('../controller/IntentController')
 
 router.post('/api/save', JSONController.save)
+
+router.post('/api/:projectId/detectIntent', IntentController.detectIntent)
 
 module.exports = router
