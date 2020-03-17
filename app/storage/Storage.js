748e730d711389b998a211f0d3e0ddae61219f55
unknown <fsantos.fps@gmail.com>
2020-03-17 10:21:34 -0300

    Criado Storage para manipulação de json

---
+++ app/storage/Storage.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const fs = require('fs')
+
+const setPath = (directory, filename) => {
+    return path.join(__dirname, '../' + directory + filename)
+}
+
+exports.save = (directory, filename, content) => {
+
+    const file = path.join(__dirname, '../' + directory + filename)
+
+    fs.writeFile(file, content, (err)=>{
+        if(err) throw err
+    })
+
+    return true;
+}
+
+exports.fileExist = (directory, filename) => {
+    const file = setPath(directory, filename)
+    return fs.existsSync(file) ? true : false
+}
+
+exports.read = (directory, filename) => {
+    if(this.fileExist(directory, filename)){
+        const rawdata = fs.readFileSync(setPath(directory, filename))
+        return JSON.parse(rawdata)
+    }
+}
