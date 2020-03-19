ecc86ddb98566d4d3957d2e0cbc85ae77c02c75a
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-03-19 10:52:02 -0300

    Retirado o search no mapping

--- app/service/Dialogflow/SessionClient.js
+++ app/service/Dialogflow/SessionClient.js
@@ -1,7 +1,6 @@
 const dialogflow = require('dialogflow')
 const uuid = require('uuid')
 const path = require('path')
-const Mapping = require('../../mapping/Mapping')
 const Credential = require('../../credentials/Credential')
 
 class SessionClient{
@@ -26,16 +25,8 @@ class SessionClient{
                 }
             }
         }
-
         const result = await this.client.detectIntent(request)
-        const mapping = new Mapping(projectId)
-        const json = mapping.getFile()
-        const block = json.intents[result[0].queryResult.intent.displayName]
-
-        return {
-            intentDetected : result[0].queryResult.intent.displayName,
-            redirect_to_blocks: [block]
-        }
+        return result[0].queryResult.intent.displayName
     }
 }
 
