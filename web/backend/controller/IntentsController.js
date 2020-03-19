c35b7070b00251ecd884230310e5a7688b3b0e5e
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-03-19 11:02:35 -0300

    Ajuste no bloco de passagem parametros

--- web/backend/controller/IntentsController.js
+++ web/backend/controller/IntentsController.js
@@ -5,22 +5,21 @@ exports.intentsList = async (req, res)=>{
 
     const projectId = req.params.id
     const token = req.session.token
-
-    const api = new Dialogflow(token)
+    const endpoint = `http://ec2-13-57-183-130.us-west-1.compute.amazonaws.com/api/${projectId}/detectIntent`
 
     const dialogflow = new Dialogflow(token)
     const projects = await dialogflow.agentList()
+    const intents = await dialogflow.intentsList(projectId)
 
-    const intents = await api.intentsList(projectId)
     const mapping = new Mapping(projectId);
-    const chatfuel = mapping.getFile(projectId)
+    const blocks = mapping.read()
     
     const params = {
         projects: projects,
         intents: intents,
         projectId: projectId,
-        chatfuel: chatfuel.intents,
-        endpoint: chatfuel.url
+        chatfuel: blocks == null ? {} : blocks,
+        endpoint: blocks == null ? '' : endpoint
     }
 
     res.render('pages/intents', {params: params})
