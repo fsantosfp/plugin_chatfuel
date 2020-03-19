fa5d405ace70d19c1da4d12b1785d6e5a4811392
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-03-19 11:05:47 -0300

    Delegado funções especificas para seus respectivos módulos

--- app/controller/IntentController.js
+++ app/controller/IntentController.js
@@ -1,11 +1,22 @@
-const DialogflowSessionClient = require('../service/Dialogflow/SessionClient')
+const DialogflowSessionClient = require('../service/dialogflow/SessionClient')
+const ChatfuelResponse = require('../responses/ChatfuelResponse')
+const Mapping = require('../mapping/Mapping')
 
 exports.detectIntent = async (req, res)=>{
     const {input} = req.body
     const projectId = req.params.projectId
 
+    //Detect a intent
     const dialogflowClient = new DialogflowSessionClient
-    const json = await dialogflowClient.detect(projectId, input);
+    const intent = await dialogflowClient.detect(projectId, input);
+    
+    // procura valor:chave da intent no mapping
+    const mapping = new Mapping(projectId)
+    const result = mapping.find(intent)
+
+    //formata retorno
+    const response = new ChatfuelResponse
+    const json = response.format(intent, result)
 
     res.json(json)
 }
