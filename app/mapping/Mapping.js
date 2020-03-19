f4b42d483d807cc9466ca4b1f5ebead60c9af055
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-03-19 11:04:32 -0300

    Inclusão da função find e refatoração das funções existentes

--- app/mapping/Mapping.js
+++ app/mapping/Mapping.js
@@ -1,45 +1,24 @@
-const fs = require('fs')
-const path = require('path')
+const Storage = require('../storage/Storage')
 
 class Mapping {
 
-    constructor(project){
-        this.path = path.join(__dirname,'../mapping/json/')
-        this.json = {
-            url: "",
-            intents: {}
-        }
-        this.url = `http://ec2-13-57-183-130.us-west-1.compute.amazonaws.com/api/${project}/detectIntent`;
-        this.project = project
+    constructor(projectId){
+        this.directory = 'mapping/json/'
+        this.fileName = `mapping-${projectId}.json`
     }
-
     save(data){
-
-        const fileName = `getbot-etl_${this.project}.json`
-        const body = JSON.stringify(data.intents)
-
-       fs.writeFile(this.path + fileName, body, function (err) {
-            if (err) throw err;
-        });
-
+        const content = JSON.stringify(data.intents)
+        Storage.save(this.directory, this.fileName, content)
         return this.url;
     }
 
-    getFile(){
-        const fileName = `getbot-etl_${this.project}.json`
-        const file = path.join(this.path, fileName);
+    read(){
+       return Storage.read(this.directory, this.fileName)
+    }
 
-        try{
-            if(fs.existsSync(file)){
-                const rawdata = fs.readFileSync(file)
-                this.json.intents = JSON.parse(rawdata)
-                this.json.url = this.url
-            }
-        }catch(error){
-            throw error
-        }
-        
-        return this.json
+    find(intent){
+        const mapping = this.read()
+        return mapping[intent] !== undefined ? mapping[intent] : ""
     }
 
 }
