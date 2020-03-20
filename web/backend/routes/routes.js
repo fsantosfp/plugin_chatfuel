556a53ea8df4576c7956c9b5ea325bc441f8067a
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-03-20 17:44:45 -0300

    teste

--- web/backend/routes/routes.js
+++ web/backend/routes/routes.js
@@ -7,6 +7,7 @@ const routes = express.Router()
 
 routes.get('/home', HomeControlle.index)
 
-routes.get('/project/:id',IntentController.intentsList)
+//routes.get('/project/:id',IntentController.intentsList)
+routes.get('/project/:id',HomeControlle.index)
 
 module.exports = routes
