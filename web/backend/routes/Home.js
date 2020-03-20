4c217b0062ac33714f776844703e658f05085518
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-03-20 17:23:54 -0300

    deletado

--- web/backend/routes/Home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const HomeControlle = require('../controller/HomeController')
-
-const routes = express.Router()
-
-routes.get('/home', HomeControlle.index)
-
-module.exports = routes
